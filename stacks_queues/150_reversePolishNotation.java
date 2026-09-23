c44fc2b44d750ba0c07d5e0de0e2cead3f6d2179
Yashika <yashikasanan98@gmail.com>
2026-09-23 20:41:35 +0530

    Update 150_reversePolishNotation.java

--- stacks_queues/150_reversePolishNotation.java
+++ stacks_queues/150_reversePolishNotation.java
@@ -1,1 +1,30 @@
-
+import java.util.*;
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String str : tokens){
+            if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")) {
+                int n2 = stack.pop();       
+                int n1 = stack.pop();
+                int res=0;
+                if (str.equals("+")){
+                    res = n1+n2;
+                }
+                else if (str.equals("-")){
+                    res = n1-n2;
+                }
+                else if (str.equals("*")){
+                    res = n1*n2;
+                }
+                else {
+                    res = n1/n2;
+                }
+                stack.push(res);
+            }
+            else {
+                stack.push(Integer.parseInt(str));
+            }
+        }
+        return stack.pop();
+    }
+}
